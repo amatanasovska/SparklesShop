fc738942cf33fb9c0eb02aaa4990359e7c860683
amatanasovska <amatanasovska@yahoo.com>
2023-08-09 21:07:34 +0200

    Initial models implementation

--- shop/models.py
+++ shop/models.py
@@ -1,3 +1,58 @@
 from django.db import models
+from django.contrib.auth.models import User
 
 # Create your models here.
+
+class ShoppingCart(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+
+class Category(models.Model):
+    name = models.CharField(max_length=255)
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    quantity = models.IntegerField()
+    price = models.IntegerField()
+    image = models.ImageField(upload_to='images')
+    description = models.TextField(max_length=255)
+    category = models.ForeignKey(Category, on_delete=models.CASCADE)
+
+class Comment(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    content = models.CharField(max_length=255)
+
+class Order(models.Model):
+    pass
+
+class OrderItem(models.Model):
+    user = models.ForeignKey(User, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE)
+    quantity = models.IntegerField()
+    order = models.ForeignKey(Order, on_delete=models.CASCADE)
+
+
+class Store(models.Model):
+    location = models.CharField(max_length=20, choices=(('SK', 'Skopje'),
+                                                        ('BT', 'Bitola'),
+                                                        ('PR', 'Prilep'),
+                                                        ('VE', 'Veles'),
+                                                        ('SR', 'Strumica')))
+
+class Availability(models.Model):
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    product = models.ForeignKey(Product, on_delete=models.CASCADE) 
+
+class ProductProperty(models.Model):
+    name = models.CharField(max_length=50)
+
+class ProductPropertiesValue(models.Model):
+   product_property = models.ForeignKey(ProductProperty, on_delete=models.CASCADE)
+   product = models.ForeignKey(Product, on_delete=models.CASCADE)
+   value = models.CharField(max_length=255)
+
+
+
+
+
--- sparklesapp/settings.py
+++ sparklesapp/settings.py
@@ -11,6 +11,9 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+import os
+
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -37,6 +40,7 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'shop'
 ]
 
 MIDDLEWARE = [
@@ -116,6 +120,10 @@ USE_TZ = True
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
 STATIC_URL = 'static/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, "data/")
+MEDIA_URL = '/data/'
+
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
