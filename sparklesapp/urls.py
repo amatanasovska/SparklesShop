9463c563fb14cc3228990ac03b0348e7b726d835
amatanasovska <amatanasovska@yahoo.com>
2023-08-09 21:46:28 +0200

    Custom login implementation

--- sparklesapp/urls.py
+++ sparklesapp/urls.py
@@ -16,7 +16,11 @@ Including another URLconf
 """
 from django.contrib import admin
 from django.urls import path
+from shop.views import *
+from django.conf.urls.static import static
+from django.conf import settings
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-]
+    path('login/', login_user, name="user_login"),
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
